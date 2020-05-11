825c0b72e3e112039feebae92af4f131eaa97410
Martin Muñoz <martin.munoz.temp@addepar.com>
2020-05-11 14:12:21 -0400

    Configure ember-try to test Ember 1.13 and 2.5 with and without ember-legacy-views

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,86 +1,40 @@
 'use strict';
 
-const getChannelURL = require('ember-source-channel-url');
-
 module.exports = async function() {
   return {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0'
-          }
+        name: 'ember-1.13-without-ember-legacy-views',
+        bower: {
+          ember: '1.13.13'
         }
       },
       {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0'
-          }
-        }
-      },
-      {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
-      },
-      // The default `.travis.yml` runs this scenario via `yarn test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: 'ember-default',
-        npm: {
-          devDependencies: {}
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
+        name: 'ember-1.13-with-ember-legacy-views',
+        bower: {
+          ember: '1.13.13'
         },
         npm: {
           devDependencies: {
-            '@ember/jquery': '^0.5.1'
+            'ember-legacy-views': '0.2.0'
           }
         }
       },
       {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false
-          })
+        name: 'ember-2.5-without-ember-legacy-views',
+        bower: {
+          ember: '2.5.1'
+        }
+      },
+      {
+        name: 'ember-2.5-with-ember-legacy-views',
+        bower: {
+          ember: '2.5.1'
         },
         npm: {
-          ember: {
-            edition: 'classic'
+          devDependencies: {
+            'ember-legacy-views': '0.2.0'
           }
         }
       }
